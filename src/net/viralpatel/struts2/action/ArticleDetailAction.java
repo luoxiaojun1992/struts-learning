64aaa85468413621abbf863db177cdeda29cd637
罗晓俊 <luoxiaojun1992@sina.cn>
2017-02-27 17:14:21 +0800

    action context

--- src/net/viralpatel/struts2/action/ArticleDetailAction.java
+++ src/net/viralpatel/struts2/action/ArticleDetailAction.java
@@ -1,5 +1,8 @@
 package net.viralpatel.struts2.action;
 
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ArticleDetailAction extends ActionSupport {
@@ -8,7 +11,12 @@ public class ArticleDetailAction extends ActionSupport {
  
     public String execute() {
  
-    	setTitle(getText("article.title"));
+    	//Get Request Parameters
+    	ActionContext context = ActionContext.getContext();
+    	Map params = context.getParameters();
+    	String articleId = params.getOrDefault("articleId", "111").toString();
+    	
+    	setTitle(getText("article.title").concat(articleId));
     	setContent(getText("article.content"));
     	return SUCCESS;
         
