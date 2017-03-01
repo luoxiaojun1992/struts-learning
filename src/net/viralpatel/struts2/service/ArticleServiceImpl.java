7311257f8428484d1dd4757a9a65e46e7630c0dd
Gogs <gogitservice@gmail.com>
2017-03-01 22:04:23 +0800

    add dao

--- src/net/viralpatel/struts2/service/ArticleServiceImpl.java
+++ src/net/viralpatel/struts2/service/ArticleServiceImpl.java
@@ -1,14 +1,26 @@
 package net.viralpatel.struts2.service;
 
 import net.viralpatel.struts2.bean.Article;
+
 import net.viralpatel.struts2.dao.ArticleDao;
+import net.viralpatel.struts2.dao.BaseDao;
 
 public class ArticleServiceImpl implements ArticleService {
 
+	private ArticleDao articleDao;
+	
 	public Article getArticleById(int id) {
 		
-		return (Article) new ArticleDao().getEntityById(Article.class, id);
+		return (Article) getArticleDao().getEntityById(Article.class, id);
 		
+	}
+	
+	public void setArticleDao(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+	
+	public BaseDao getArticleDao() {
+		return articleDao;
 	}
 	
 }
