53bc2f2a97f372a167cf27bbe9651ce3bbe5bed4
罗晓俊 <luoxiaojun1992@sina.cn>
2017-03-03 13:54:18 +0800

    add article list

--- src/net/viralpatel/struts2/action/ArticleListAction.java
+++ src/net/viralpatel/struts2/action/ArticleListAction.java
@@ -1,5 +1,45 @@
 package net.viralpatel.struts2.action;
 
-public class ArticleListAction {
+import java.util.List;
 
+import net.viralpatel.struts2.bean.Article;
+import net.viralpatel.struts2.service.ArticleService;
+
+public class ArticleListAction extends BaseAction {
+
+	private ArticleService articleServ;
+	private List<Article> articles;
+	
+	public String execute() {
+    	
+    	setArticles(getArticleServ().getAllArticles());
+    	
+    	return super.execute();
+        
+    }
+    
+    public void setArticleServ(ArticleService articleServ) {
+    	
+    	this.articleServ = articleServ;
+    
+    }
+    
+    public ArticleService getArticleServ() {
+    	
+    	return articleServ;
+    
+    }
+    
+    public void setArticles(List<Article> articles) {
+    	
+    	this.articles = articles;
+    
+    }
+    
+    public List<Article> getArticles() {
+    	
+    	return articles;
+    	
+    }
+	
 }
