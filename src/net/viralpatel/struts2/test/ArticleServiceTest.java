219df98109a2e2f2bd55d87e67cb3e7402338831
罗晓俊 <luoxiaojun1992@sina.cn>
2017-03-09 18:45:46 +0800

    modify unit test

--- src/net/viralpatel/struts2/test/ArticleServiceTest.java
+++ src/net/viralpatel/struts2/test/ArticleServiceTest.java
@@ -11,13 +11,20 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+
+import net.viralpatel.struts2.bean.Article;
+import net.viralpatel.struts2.dao.ArticleDao;
+import net.viralpatel.struts2.service.ArticleServiceImpl;
 
 /**
  * @author luoxiaojun
  *
  */
 public class ArticleServiceTest {
-
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -48,7 +55,13 @@ public class ArticleServiceTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ArticleDao articleDao = mock(ArticleDao.class);
+		ArticleServiceImpl articleService  = new ArticleServiceImpl();
+		articleService.setArticleDao(articleDao);
+		Article article = new Article();
+		article.setId(1);
+		when(articleDao.getArticleById(1)).thenReturn(article);
+		assertEquals(articleService.getArticleById(1).getId(), 1);
 	}
 
 }
